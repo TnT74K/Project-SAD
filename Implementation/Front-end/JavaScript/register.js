932b362ae064a0556f3131b20e772be4091e41ac
Hamed <hamed9772yazdizadeh@gmail.com>
2026-06-02 08:28:12 -0600

    seperating html,css,js for register

---
+++ Implementation/Front-end/JavaScript/register.js
@@ -0,0 +1,12 @@
+function togglePasswordVisibility(inputId, icon) {
+    const input = document.getElementById(inputId);
+
+    if (input.type === 'password') {
+            input.type = 'text';
+                icon.textContent = '🙈';
+            }
+             else {
+                input.type = 'password';
+                icon.textContent = '👁';
+        }
+}
