2737f87e554fc36080ec49464c061220d84a3be2
AbolfazlVaziri1384 <Abolfazlvaziri006@gmail.com>
2026-06-05 17:26:46 +0330

    Add Header and Footer html , css , and js files

---
+++ Implementation/Front-end/JavaScript/Header.js
@@ -0,0 +1,35 @@
+function initHeader(){
+
+const header = document.getElementById("header");
+const hamburgerBtn = document.getElementById("hamburgerBtn");
+const mobileMenu = document.getElementById("mobileMenu");
+
+window.addEventListener("scroll", () => {
+
+if(!header) return;
+
+if (window.scrollY > 30) {
+header.classList.add("scrolled");
+}else{
+header.classList.remove("scrolled");
+}
+
+});
+
+if(hamburgerBtn && mobileMenu){
+
+hamburgerBtn.addEventListener("click", () => {
+mobileMenu.classList.toggle("open");
+});
+
+document.addEventListener("click", (e) => {
+
+if(!header.contains(e.target)){
+mobileMenu.classList.remove("open");
+}
+
+});
+
+}
+
+}
