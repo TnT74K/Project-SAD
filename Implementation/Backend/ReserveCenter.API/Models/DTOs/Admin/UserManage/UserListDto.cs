8a09eb954c320ffee6276f29d12b2f8f12e13d7c
TnT74K <sajad.sabaghkermani@icloud.com>
2026-07-09 22:23:25 +0330

    Add method names as comments

--- Implementation/Backend/ReserveCenter.API/Models/DTOs/Admin/UserManage/UserListDto.cs
+++ Implementation/Backend/ReserveCenter.API/Models/DTOs/Admin/UserManage/UserListDto.cs
@@ -3,9 +3,9 @@
     public class UserListDto
     {
         public int Id { get; set; }
-        public string FullName { get; set; }           // نام + نام خانوادگی
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
         public string PhoneNumber { get; set; }
-        public string Role { get; set; }               // نقش کاربر
         public bool IsBlocked { get; set; }
         public bool IsDeleted { get; set; }
         public DateTime CreatedDate { get; set; }
---
+++ Implementation/Backend/ReserveCenter.API/Services/Interfaces/IAdminUserListService.cs
@@ -0,0 +1,13 @@
+using ReserveCenter.API.Models.DTOs;
+namespace ReserveCenter.API.Services.Interfaces
+{
+    public interface IAdminUserList
+    {
+        // SuspendUser
+        // ActivateUSer
+        // changeUserData
+        // getAllUsers
+        // getUserDetail
+    }
+
+}
