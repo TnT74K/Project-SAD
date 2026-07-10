06ab2f0a05c3156ea5d25564d01246f90bcd380e
TnT74K <sajad.sabaghkermani@icloud.com>
2026-07-10 22:29:45 +0330

    Implement controller structure and one endpoint

--- Implementation/Backend/ReserveCenter.API/Controllers/Admin/AdminUserListController.cs
+++ Implementation/Backend/ReserveCenter.API/Controllers/Admin/AdminUserListController.cs
@@ -1,1 +1,25 @@
-// GET /api/admin/users
+using Microsoft.AspNetCore.Mvc;
+using ReserveCenter.API.Services.Interfaces;
+namespace ReserveCenter.API.Controllers.Admin
+{
+    [ApiController] // We'll put this on almost every API controller.
+    [Route("api/admin/users")]
+    public class AdminUserListController : ControllerBase
+    {
+        private readonly IAdminUserListService _adminUserListService;
+
+        public AdminUserListController(IAdminUserListService adminUserListService)
+        {
+            _adminUserListService = adminUserListService;
+        }
+
+        // These are called 'endpoints'
+        [HttpGet]
+        public async Task<IActionResult> GetAllUsers()
+        {
+            var users = await _adminUserListService.GetAllUsersAsync();
+
+            return Ok(users);
+        }
+    }
+}
